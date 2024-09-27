6719994a9a9d262fd50e80454d9937090c95ad93
Joyline Rencita Dsouza <143104781+Joyline-Rencita-Dsouza@users.noreply.github.com>
2024-09-27 11:03:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Animation Script for display
+const animationScript = ["Final Year CSE Student", "Front End Developer", "React.js Developer", "Full Stack Developer"];
+
+// Typing animation script start
+new Typed(".typing", {
+  strings: animationScript,
+  typeSpeed: 100,    // Speed for typing
+  backSpeed: 60,     // Speed for backspacing
+  loop: true         // Loop the typing animation
+});
+
+new Typed(".typing2", {
+  strings: animationScript,
+  typeSpeed: 100,    // Speed for typing
+  backSpeed: 60,     // Speed for backspacing
+  loop: true         // Loop the typing animation
+});
+// Typing animation script End
